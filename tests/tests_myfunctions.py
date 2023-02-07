70eebee457cf84a1e49fa8735fd61dd3919e4057
LibroGit <alireza.ketabdari@mail.polimi.it>
2023-02-07 14:56:59 +0100

    making the function easier

--- tests/tests_myfunctions.py
+++ tests/tests_myfunctions.py
@@ -4,8 +4,4 @@ def test_haversine():
     assert myfunctions.haversine(52.370216, 4.895168, 52.520008,13.404954) == 945793.4375088713
     print("jjjjj")
 
-def main():
-    test_haversine()
-
-if __name__ == "__main__":
-    main()
+assert myfunctions.haversine(52.370216, 4.895168, 52.520008,13.404954) == 945793.4375088713
