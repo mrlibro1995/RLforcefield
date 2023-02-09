67d5eb80489ec3b96e17f67380d2d6745493e0bf
alireza <alireza.ketabdari@mail.polimi.it>
2023-02-09 18:27:03 +0100

    add sensitivity file

---
+++ rlff/sensitivity.py
@@ -0,0 +1,15 @@
+import gromologist as gml
+import numpy as np
+
+td = gml.ThermoDiff()
+# this adds all possible NBFIXes to the list of calculated sensitivities:
+td.add_all_sigma_mods(top='merged_topology.top',
+        structure='minimized_structure.pdb')
+# this specifies a trajectory on which the sensitivity will be calculated, as well as relevant datasets:
+hdata = np.loadtxt('helix.dat')[:,1]
+td.add_traj(top='merged_topology.top', traj='output_traj1.xtc', datasets={'helicity': hdata})
+td.run() # this part will take some time
+# let's find the difference between the binned derivatives for the lower and upper half of the dataset:
+hmin, hmax = np.min(hdata), np.max(hdata)
+hmid = 0.5 * (hmin + hmax)
+td.calc_discrete_derivatives(dataset='helicity', threshold=[hmin, hmid, hmid, hmax])
