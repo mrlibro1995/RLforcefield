86c7e795e21263e6a1d50cfd767871fdf3e3afb6
alireza <alireza.ketabdari@mail.polimi.it>
2023-02-16 15:31:00 +0100

    adding helix function

--- rlff/sensitivity.py
+++ rlff/sensitivity.py
@@ -1,24 +1,34 @@
 import gromologist as gml
 import numpy as np
+import plumed
+import os
 
 
-def claculate (topfile, pdbfile, xtcfile, datfile, filedir):
-        td = gml.ThermoDiff()
-        # this adds all possible NBFIXes to the list of calculated sensitivities:
-        td.add_all_sigma_mods(top= topfile,
-        structure=pdbfile, exclude=['OW', 'HW'])
-        # this specifies a trajectory on which the sensitivity will be calculated, as well as relevant datasets:
-        hdata = np.loadtxt(datfile)[:,1]
-        td.add_traj(top=topfile, traj=xtcfile, datasets={'helicity': hdata})
-        td.run() # this part will take some time
-        # let's find the difference between the binned derivatives for the lower and upper half of the dataset:
-        hmin, hmax = np.min(hdata), np.max(hdata)
-        hmid = 0.5 * (hmin + hmax)
-        td.calc_discrete_derivatives(dataset='helicity', threshold=[hmin, hmid, hmid, hmax])
-        for key in td.discrete_free_energy_derivatives.keys():
-                print(key, td.discrete_free_energy_derivatives[key][1] - td.discrete_free_energy_derivatives[key][0])
+def sensitivity_claculate(topfile, pdbfile, xtcfile, datfile, filedir):
+    td = gml.ThermoDiff()
+    # this adds all possible NBFIXes to the list of calculated sensitivities:
+    td.add_all_sigma_mods(top=topfile,
+                          structure=pdbfile, exclude=['OW', 'HW'])
+    # this specifies a trajectory on which the sensitivity will be calculated, as well as relevant datasets:
+    hdata = np.loadtxt(datfile)[:, 1]
+    td.add_traj(top=topfile, traj=xtcfile, datasets={'helicity': hdata})
+    td.run()  # this part will take some time
+    # let's find the difference between the binned derivatives for the lower and upper half of the dataset:
+    hmin, hmax = np.min(hdata), np.max(hdata)
+    hmid = 0.5 * (hmin + hmax)
+    td.calc_discrete_derivatives(dataset='helicity', threshold=[hmin, hmid, hmid, hmax])
+    for key in td.discrete_free_energy_derivatives.keys():
+        print(key, td.discrete_free_energy_derivatives[key][1] - td.discrete_free_energy_derivatives[key][0])
 
-        # this shows the identities of the modifications:
-        print([(i.type, str(i)) for i in td.mods])
+    # this shows the identities of the modifications:
+    print([(i.type, str(i)) for i in td.mods])
+
+
+def helix_calculation(xtcfile, datfile, pdbfile):  #
+    print(xtcfile)
+    print(datfile)
+    print(pdbfile)
+    print("plumed driver --mf_xtc " + xtcfile + " --plumed " + datfile + " --pdb " + pdbfile)
+    os.system("plumed driver --mf_xtc " + xtcfile + " --plumed " + datfile + " --pdb " + pdbfile)
 
 
--- tests/sesitivitytest.py
+++ tests/sesitivitytest.py
@@ -1,11 +1,12 @@
 from RLforcefield.rlff import sensitivity, filemanager
 
-#top_path, pdb_path, xtc_path, dat_path = filemanager.sensitivity_file_manager()
+# top_path, pdb_path, xtc_path, dat_path = filemanager.sensitivity_file_manager()
 top_path = "001_top.top"
 pdb_path = "001_pdb.pdb"
 xtc_path = "001_trajectory.xtc"
 dat_path = "001_helix.dat"
-filedir  = '/home/alireza/Desktop/myprojects/irbproject/rlff/RLforcefield/tests/samples'
+plumed_path = "plumed.dat"
+filedir = '/home/alireza/Desktop/myprojects/irbproject/rlff/RLforcefield/tests/samples'
 
-
-sensitivity.claculate(top_path, pdb_path, xtc_path, dat_path, filedir)
+# sensitivity.sensitivity_claculate(top_path, pdb_path, xtc_path, dat_path, filedir)
+sensitivity.helix_calculation(xtc_path, plumed_path, pdb_path)
