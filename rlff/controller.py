2008c730c7be012c2e1a4c188762a8df4138144a
mrlibro <alireza.ketabdari@mail.polimi.it>
2023-03-08 19:07:32 +0100

    some smail changes

--- rlff/controller.py
+++ rlff/controller.py
@@ -5,15 +5,16 @@ class Controller:
     def __init__(self, system: SystemObj):
         self.sys = system
 
-    def systemmodifier(self, atom: str, step: float = 0.01, attr: str = "sigma"):
+    def systemmodifier(self, id: int, atom: str, step: float = 0.01, ns: int =10 , attr: str = "sigma"):
         topo = gml.Top(self.sys.topo, pdb=self.sys.pdb)
         topo.check_pdb()
         if (attr == "sigma"):
             topo.parameters.edit_atomtype(atom, mod_sigma=step)
-        topo.save_top("newtopo.top")
-        topo.pdb.save_pdb("newpdb.pdb")
-        newsys = SystemObj("newtopo.top", "newpdb.pdb", 1)
-        newsys.trajectory_producer(newsys.topo, newsys.pdb, newsys.num)
+        print(str(id) + "_" + str(atom) + "_" + str(step) + ".top")
+        topo.save_top(str(id) + "_" + str(atom) + "_" + str(step) + ".top")
+        topo.pdb.save_pdb(str(id) + "_" + str(atom) + "_" + str(step) + ".pdb")
+        newsys = SystemObj(str(id) + "_" + str(atom) + "_" + str(step) + ".top", str(id) + "_" + atom + "_" + str(step) + ".pdb", 1)
+        newsys.trajectory_producer(newsys.topo, newsys.pdb, newsys.num,duration_ns= ns)
 
     def sensitive_atoms(self, hel_atoms, n_top):
         '''''''''''''''''''''''''''''''''''''''''''''''
