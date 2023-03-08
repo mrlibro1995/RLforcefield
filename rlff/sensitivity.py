8c3b0934277293adc9d5c3c1a5ca60c35dc7e473
mrlibro <alireza.ketabdari@mail.polimi.it>
2023-03-08 14:41:35 +0100

    some smail changes

--- rlff/sensitivity.py
+++ rlff/sensitivity.py
@@ -39,3 +39,4 @@ def sensitivity_calculate(topfile, pdbfile, xtcfile, datfile, filedir, exclude):
     print([(i.type, str(i)) for i in td.mods])
 '''''
 
+"sapowkdpqwodkpqowkdpoqwkdopqwkd"
--- tests/systemtest.py
+++ tests/systemtest.py
@@ -4,9 +4,9 @@ from RLforcefield.rlff import controller
 s = system.SystemObj("001_top.top", "001_pdb.pdb",0)
 c = controller.Controller()
 
-s.trajectory_producer(s.topo,s.pdb,s.num)
-s.helicity_calc("001_pdb.pdb", "001_trajectory.xtc", "plumed.dat")
+#s.trajectory_producer(s.topo,s.pdb,s.num)
+#s.helicity_calc("001_pdb.pdb", "001_trajectory.xtc", "plumed.dat")
 helicityatoms = s.sensitivity_calc("001_top.top","001_pdb.pdb","001_trajectory.xtc","001_helix.dat",['OW', 'HW'])
-c.sensitive_atoms(helicityatoms, 5)
-system.systemmodifier(s,'C', 0.02)
+#c.sensitive_atoms(helicityatoms, 5)
+#system.systemmodifier(s,'C', 0.02)
 
