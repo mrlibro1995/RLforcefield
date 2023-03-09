6b7cfa82f24489c6f584b8f52a63d2be11c4efc9
mrlibro <alireza.ketabdari@mail.polimi.it>
2023-03-09 19:43:08 +0100

    some smail changes

--- rlff/controller.py
+++ rlff/controller.py
@@ -5,15 +5,15 @@ class Controller:
     def __init__(self, system: SystemObj):
         self.sys = system
 
-    def systemmodifier(self, id: int, atom: str, step: float = 0.01, ns: int =10 , attr: str = "sigma"):
+    def systemmodifier(self, id: int, atom: str, change: float = 0.01, ns: int =10 , para: str = "sigma"):
         topo = gml.Top(self.sys.topo, pdb=self.sys.pdb)
         topo.check_pdb()
-        if (attr == "sigma"):
-            topo.parameters.edit_atomtype(atom, mod_sigma=step)
-        print(str(id) + "_" + str(atom) + "_" + str(step) + ".top")
-        topo.save_top(str(id) + "_" + str(atom) + "_" + str(step) + ".top")
-        topo.pdb.save_pdb(str(id) + "_" + str(atom) + "_" + str(step) + ".pdb")
-        newsys = SystemObj(str(id) + "_" + str(atom) + "_" + str(step) + ".top", str(id) + "_" + atom + "_" + str(step) + ".pdb", 1)
+        if (para == "sigma"):
+            topo.parameters.edit_atomtype(atom, mod_sigma=change)
+        print(str(id) + "_" + str(atom) + "_" + str(change) + ".top")
+        topo.save_top(str(id) + "_" + str(atom) + "_" + str(change) + ".top")
+        topo.pdb.save_pdb(str(id) + "_" + str(atom) + "_" + str(change) + ".pdb")
+        newsys = SystemObj(str(id) + "_" + str(atom) + "_" + str(change) + ".top", str(id) + "_" + atom + "_" + str(change) + ".pdb", 1)
         newsys.trajectory_producer(newsys.topo, newsys.pdb, newsys.num,duration_ns= ns)
 
     def sensitive_atoms(self, hel_atoms, n_top):
