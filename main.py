caabe029e8eccedbdff6e225c7e460d0b61bf6a8
mrlibro <alireza.ketabdari@mail.polimi.it>
2023-08-23 17:27:21 +0200

    The process run well, just the results should be check and analysed
    complete and without error run
    Good check point

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ Alpha_gr = 0.03
 time_constant = 0.05  # nano-second
 run_time = time_constant
 sensitivity_counter = 1
+access_flag = 0  # 0 = full access login,  1 = low access login
 
 ### Q-function Initialization
 Alpha_qf = 400
@@ -57,8 +58,9 @@ helix_atoms = init_sys.sensitivity_calc("v2_traj.xtc", "v2_helix.dat",
                                         ['OW', 'HW', 'Cl', 'K'])
 top_sensitive_atoms, gradients = init_sys.sensitive_atoms(helix_atoms, n_atoms)
 gradients = [x * -Alpha_gr for x in gradients]
-print(f"gradients: {gradients}")
 action = qfunc.gradients2action_convertor(gradients)
+info_dic['sensitivity_list'].append([qfunc.current_location, gradients])
+print(f"gradients: {gradients}")
 print(f"actions: {action}")
 
 vs.init_system_visualization(n_atoms, global_radius, local_radius, gradients, Alpha_gr, Alpha_qf, Gamma_qf,
@@ -97,18 +99,24 @@ while id < 200:
         do_senstitivity, gradients = sys.should_calculate_sensitivity(current_location=qfunc.current_location,
                                                                       info_dic=info_dic, threshold=8)
         if do_senstitivity:
+            sensitivity_counter += 1
             print("********  do sensitivity  ********")
+            print("previous sensitivity calculations")
+            for sublist in info_dic['sensitivity_list']:
+                print(f"location: {sublist[0]} -- values: {sublist[1]}")
+
             directory = f'sensitivity{sensitivity_counter}_xtc'
             it_path = os.path.join(parent_dir, directory)
             os.mkdir(it_path)
             sys.trajectory_producer(id=0, duration_ns=10.0, path=it_path)
             sys.helix_reward_calc(it_path + "/" + f'output_traj0.xtc', it_path, time_constant, run_time)
             helix_atoms = sys.sensitivity_calc(xtc=it_path + "/" + 'output_traj0.xtc',
-                                               helicity=it_path + "/" + 'helix0.dat', exclude=
-                                               ['OW', 'HW', 'Cl', 'K'])
+                                               helicity=it_path + "/" + 'helix0.dat',
+                                               exclude=['OW', 'HW', 'Cl', 'K'])
             top_sensitive_atoms, gradients = init_sys.sensitive_atoms(helix_atoms, n_atoms)
             gradients = [x * -Alpha_gr for x in gradients]
             print(f"new gradients: {gradients}")
+            info_dic['sensitivity_list'].append([qfunc.current_location, gradients])
             next_action = qfunc.gradients2action_convertor(gradients)
             next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
             changes = qfunc.action2changes_convertor(next_action)
