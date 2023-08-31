6506ed537527e396052392dd203597f8fb71f96a
mrlibro <alireza.ketabdari@mail.polimi.it>
2023-08-31 03:35:00 +0200

    The process run well, just the results should be check and analysed
    complete and without error run
    Good check point

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ global_radius = 5
 local_radius = 2
 id = 2
 grid_step = 0.005
-initial_qval = 12
+initial_qval = 14
 init_sys = s.SystemObj("v2_top.top", "v2_pdb.pdb", id)
 qfunc = qf.Q_function(n_atoms, global_radius, local_radius, grid_step=grid_step, initial_qval=initial_qval)
 Alpha_gr = 0.03
-time_constant = 1.0  # nano-second
-run_time = time_constant
+time_constant = 4.0  # nano-second
+run_time = time_constant + 0.5
 sensitivity_counter = 1
 access_flag = 0  # 0 = full access login,  1 = low access login
 
@@ -81,13 +81,13 @@ vs.runtime_visualizarion(id, info_dic, "Grad", action, data, reward, it_path)
 
 id = 3
 
-while id < 200:
+while id < 500:
 
     directory = f'it_{id}'
     it_path = os.path.join(parent_dir, directory)
     os.mkdir(it_path)
 
-    random_number = 0.2  # random.random()
+    random_number = random.random()
     print(f"Epsilon Random Number: {random_number}")
     if random_number > 0.3:  ### Walk based on RL decision
         next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
@@ -96,44 +96,49 @@ while id < 200:
         print(f"QF Based Walk with: {changes}")
 
     elif random_number > 0.1 and random_number <= 0.3:  ### Walk based on Gradient Discent
-        do_senstitivity, gradients = sys.should_calculate_sensitivity(current_location=qfunc.current_location,
-                                                                      info_dic=info_dic, threshold=8)
-        do_senstitivity = True
-        if do_senstitivity:
-            sensitivity_counter += 1
-            print("previous sensitivity calculations")
-            for sublist in info_dic['sensitivity_list']:
-                print(f"location: {sublist[0]} -- values: {sublist[1]}")
-            print("********  do sensitivity  ********")
-
-            directory = f'sensitivity{sensitivity_counter}_xtc'
-            it_path = os.path.join(parent_dir, directory)
-            os.mkdir(it_path)
-
-            sys.trajectory_producer(id=0, duration_ns=6.0, path=it_path, plumed_file="plumed_sens.dat")
-            sys.helix_reward_calc(xtc=it_path + "/" + f'output_traj0.xtc', dir=it_path, time_constant=time_constant,
-                                  run_time=run_time, sensitivity=1)
-            os.chdir(it_path)
-            helix_atoms = sys.sensitivity_calc(xtc=it_path + "/" + 'output_traj0.xtc',
-                                               helicity=it_path + "/" + 'helix_sens.dat',
-                                               exclude=['OW', 'HW', 'Cl', 'K'])
-            os.chdir('..')
-            top_sensitive_atoms, gradients = init_sys.sensitive_atoms(helix_atoms, n_atoms)
-            gradients = [x * -Alpha_gr for x in gradients]
-            print(f"new gradients: {gradients}")
-            info_dic['sensitivity_list'].append([qfunc.current_location, gradients])
-            next_action = qfunc.gradients2action_convertor(gradients)
-            next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
-            changes = qfunc.action2changes_convertor(next_action)
-            action_type = "Grad"
-            print("********  Finished sensitivity  ********")
-        else:
-            print("******** NOT  do sensitivity  ********")
-            next_action = qfunc.gradients2action_convertor(gradients)
-            next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
-            changes = qfunc.action2changes_convertor(next_action)
-            action_type = "Grad"
-            print(f"Gradients Based Walk with: {changes}")
+        # do_senstitivity, gradients = sys.should_calculate_sensitivity(current_location=qfunc.current_location,
+        #                                                               info_dic=info_dic, threshold=8)
+        # do_senstitivity = True
+        # if do_senstitivity:
+        #     sensitivity_counter += 1
+        #     print("previous sensitivity calculations")
+        #     for sublist in info_dic['sensitivity_list']:
+        #         print(f"location: {sublist[0]} -- values: {sublist[1]}")
+        #     print("********  do sensitivity  ********")
+        #
+        #     directory = f'sensitivity{sensitivity_counter}_xtc'
+        #     it_path = os.path.join(parent_dir, directory)
+        #     os.mkdir(it_path)
+        #
+        #     sys.trajectory_producer(id=0, duration_ns=6.0, path=it_path, plumed_file="plumed_sens.dat")
+        #     sys.helix_reward_calc(xtc=it_path + "/" + f'output_traj0.xtc', dir=it_path, time_constant=time_constant,
+        #                           run_time=run_time, sensitivity=1)
+        #     os.chdir(it_path)
+        #     helix_atoms = sys.sensitivity_calc(xtc=it_path + "/" + 'output_traj0.xtc',
+        #                                        helicity=it_path + "/" + 'helix_sens.dat',
+        #                                        exclude=['OW', 'HW', 'Cl', 'K'])
+        #     os.chdir('..')
+        #     top_sensitive_atoms, gradients = init_sys.sensitive_atoms(helix_atoms, n_atoms)
+        #     gradients = [x * -Alpha_gr for x in gradients]
+        #     print(f"new gradients: {gradients}")
+        #     info_dic['sensitivity_list'].append([qfunc.current_location, gradients])
+        #     next_action = qfunc.gradients2action_convertor(gradients)
+        #     next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
+        #     changes = qfunc.action2changes_convertor(next_action)
+        #     action_type = "Grad"
+        #     print("********  Finished sensitivity  ********")
+        # else:
+        #     print("******** NOT  do sensitivity  ********")
+        #     next_action = qfunc.gradients2action_convertor(gradients)
+        #     next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
+        #     changes = qfunc.action2changes_convertor(next_action)
+        #     action_type = "Grad"
+        #     print(f"Gradients Based Walk with: {changes}")
+        next_action = qfunc.gradients2action_convertor(gradients)
+        next_action = sys.adjust_tuple_to_avoid_negatives(next_action, qfunc.current_location, global_radius)
+        changes = qfunc.action2changes_convertor(next_action)
+        action_type = "Grad"
+        print(f"Gradients Based Walk with: {changes}")
 
     else:  #### Walk based on Randomness
         next_action = tuple(random.randint(-local_radius, local_radius) for _ in range(n_atoms))
